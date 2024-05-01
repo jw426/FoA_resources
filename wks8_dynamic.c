de37103f108522f377a0c295a86608c64e4847ff
Jiwon Bae <jiwon.bae1@unimelb.edu.au>
2024-05-01 15:22:27 +1000

    string set dupe

--- wks8_dynamic.c
+++ wks8_dynamic.c
@@ -4,22 +4,46 @@
 #include <assert.h>
 
 char *string_dupe(char *s);
+char **string_set_dupe(char **S);
 
 int 
 main(int argc, char** argv) {
+    // ["a string", "dfjds", ..., "\0"]
 
-    // argv = [char*, char*, char*, char*, ... ]
-    // argc number of elements 
+
+    // A = realloc(A, sizeof(int)*(max*3))
+    // tradeoff time / space 
+    // 1. malloc - sizeof(int)*(max*2)
+    // 2. copies over the content 
+    // 3. frees the original array 
+
+
+    char** copied = string_set_dupe(argv);
     for (int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
+        printf("%s\n", copied[i]);
+        free(copied[i]);
     }
 
-    char *p = "This is a string!";
-    char* copied = string_dupe(p);
-    printf("%s\n", copied);
-
     free(copied);
+    // free(copied);
     return 0;
+}
+
+char **string_set_dupe(char **S) {
+
+    // ["a string", "dfjds", ...,]
+    int i = 0;
+    while (S[i++]);
+    char** copied_set = malloc(sizeof(char*)*i);
+    assert(copied_set);
+
+    i = 0; 
+    while (S[i]) {
+        copied_set[i] = string_dupe(S[i]);
+        i++;
+    }
+
+    return copied_set; 
 }
 
 char *string_dupe(char *s) {
