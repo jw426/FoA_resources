3f7918a7da30a588c105cd7ed4b0764ba7f60ab7
Jiwon Bae <jiwon.bae1@unimelb.edu.au>
2024-05-01 14:46:38 +1000

    malloc draft

--- wks8_dynamic.c
+++ wks8_dynamic.c
@@ -1,30 +1,36 @@
-// Your code here
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h> // strcpy
 #include <assert.h>
 
 char *string_dupe(char *s);
 
-int main(int argc, char** argv) {
+int 
+main(int argc, char** argv) {
 
-    // strcpy(char* destination, char* source, );
-    char* source = "This is the original string.";
-    char* copied = string_dupe(source);
+    // argv = [char*, char*, char*, char*, ... ]
+    // argc number of elements 
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+
+    char *p = "This is a string!";
+    char* copied = string_dupe(p);
     printf("%s\n", copied);
 
     free(copied);
     return 0;
 }
 
-
 char *string_dupe(char *s) {
 
+    // sizeof(char)*(strlen(p)+1)
+    // 1*(strlen + null byte)
+    // char* copied = malloc(sizeof(char)*(strlen(s)+1));
+    // another 
     char* copied; 
     copied = (char*)malloc(sizeof(char)*(strlen(s)+1));
-    assert(NULL);
-    
-    // char* copied = malloc(sizeof(char)*(strlen(s)+1));
+    assert(copied);
 
     int i = 0; 
     while (s[i]) {
@@ -32,6 +38,6 @@ char *string_dupe(char *s) {
         i++;
     }
     copied[i] = '\0';
-    
+
     return copied; 
 }
